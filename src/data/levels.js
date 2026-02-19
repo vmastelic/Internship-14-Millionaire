98610cd7579df772465fe7dd3fff094c804b276d
Vatroslav Mastelić <vmaste00@fesb.hr>
2026-02-19 18:10:54 +0100

    Add levels and queations

---
+++ src/data/levels.js
@@ -0,0 +1,12 @@
+export const LEVELS = [
+    { level: 1, prize: 100 },
+    { level: 2, prize: 200 },
+    { level: 3, prize: 500 },
+    { level: 4, prize: 1000 },
+    { level: 5, prize: 5000, safe: true },
+    { level: 6, prize: 10000 },
+    { level: 7, prize: 25000 },
+    { level: 8, prize: 50000 },
+    { level: 9, prize: 100000 },
+    { level: 10, prize: 500000 },
+];
---
+++ src/data/questions.js
@@ -0,0 +1,112 @@
+export const QUESTIONS = [
+    {
+        id: 1,
+        text: "Koji je glavni grad Hrvatske?",
+        answers: [
+            {id: "a", text: "Split", correct: false},
+            {id: "b", text: "Zadar", correct: false},
+            {id: "c", text: "Zagreb", correct: true},
+            {id: "d", text: "Dubrovnik", correct: false},
+        ],
+    },
+    {
+        id: 2,
+        text: "Koji je glavni grad Hrvatske?",
+        answers: [
+            {id: "a", text: "Split", correct: false},
+            {id: "b", text: "Zadar", correct: false},
+            {id: "c", text: "Zagreb", correct: true},
+            {id: "d", text: "Dubrovnik", correct: false},
+        ],
+    },
+    {
+        id: 3,
+        text: "Koji je glavni grad Hrvatske?",
+        answers: [
+            {id: "a", text: "Split", correct: false},
+            {id: "b", text: "Zadar", correct: false},
+            {id: "c", text: "Zagreb", correct: true},
+            {id: "d", text: "Dubrovnik", correct: false},
+        ],
+    },
+    {
+        id: 4,
+        text: "Koji je glavni grad Hrvatske?",
+        answers: [
+            {id: "a", text: "Split", correct: false},
+            {id: "b", text: "Zadar", correct: false},
+            {id: "c", text: "Zagreb", correct: true},
+            {id: "d", text: "Dubrovnik", correct: false},
+        ],
+    },
+    {
+        id: 5,
+        text: "Koji je glavni grad Hrvatske?",
+        answers: [
+            {id: "a", text: "Split", correct: false},
+            {id: "b", text: "Zadar", correct: false},
+            {id: "c", text: "Zagreb", correct: true},
+            {id: "d", text: "Dubrovnik", correct: false},
+        ],
+    },
+    {
+        id: 6,
+        text: "Koji je glavni grad Hrvatske?",
+        answers: [
+            {id: "a", text: "Split", correct: false},
+            {id: "b", text: "Zadar", correct: false},
+            {id: "c", text: "Zagreb", correct: true},
+            {id: "d", text: "Dubrovnik", correct: false},
+        ],
+    },
+    {
+        id: 7,
+        text: "Koji je glavni grad Hrvatske?",
+        answers: [
+            {id: "a", text: "Split", correct: false},
+            {id: "b", text: "Zadar", correct: false},
+            {id: "c", text: "Zagreb", correct: true},
+            {id: "d", text: "Dubrovnik", correct: false},
+        ],
+    },
+    {
+        id: 8,
+        text: "Koji je glavni grad Hrvatske?",
+        answers: [
+            {id: "a", text: "Split", correct: false},
+            {id: "b", text: "Zadar", correct: false},
+            {id: "c", text: "Zagreb", correct: true},
+            {id: "d", text: "Dubrovnik", correct: false},
+        ],
+    },
+    {
+        id: 9,
+        text: "Koji je glavni grad Hrvatske?",
+        answers: [
+            {id: "a", text: "Split", correct: false},
+            {id: "b", text: "Zadar", correct: false},
+            {id: "c", text: "Zagreb", correct: true},
+            {id: "d", text: "Dubrovnik", correct: false},
+        ],
+    },
+    {
+        id: 10,
+        text: "Koji je glavni grad Hrvatske?",
+        answers: [
+            {id: "a", text: "Split", correct: false},
+            {id: "b", text: "Zadar", correct: false},
+            {id: "c", text: "Zagreb", correct: true},
+            {id: "d", text: "Dubrovnik", correct: false},
+        ],
+    },
+    {
+        id: 11,
+        text: "Koji je glavni grad Hrvatske?",
+        answers: [
+            {id: "a", text: "Split", correct: false},
+            {id: "b", text: "Zadar", correct: false},
+            {id: "c", text: "Zagreb", correct: true},
+            {id: "d", text: "Dubrovnik", correct: false},
+        ],
+    },
+];
