9b39ffbe73685008c2e46c59ba5836271da7e01c
Vatroslav Mastelić <vmaste00@fesb.hr>
2026-02-20 12:01:21 +0100

    Add button color after answer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useMemo, useState } from 'react'
-import './App.css'
+import './style/App.css'
 import { QUESTIONS } from './data/questions'
+import './style/answers.css'
 
 
 function randomQuestions(questions){
@@ -11,6 +12,9 @@ function randomQuestions(questions){
 function App() {
   const [status, setStatus] = useState("start");
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  const [reveal, setReveal] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
 
   const gameQuestions = useMemo(() => randomQuestions(QUESTIONS), [status]);
 
@@ -33,44 +37,68 @@ function App() {
   }
   
   if (status === "finished") {
-      return (
-        <>
-          <h1>Kraj igre</h1>
-          <button onClick={playAgain}>Natrag na start</button>
-        </>
-      );
-    }
+    return (
+      <>
+        <h1>Kraj igre</h1>
+        <button onClick={playAgain}>Natrag na start</button>
+      </>
+    );
+  }
 
+  if (status === "winner") {
+    return (
+      <>
+        <h1>Pobjeda!</h1>
+        <button onClick={startGame}>Igraj ponovo</button>
+      </>
+    );
+  }
+  
   const handleAnswer = (answer) => {
     const isCorrect = answer.correct;
+    if(reveal)return;
+
+    setSelectedId(answer.id);
+    setReveal(true);
 
     setTimeout(() => {
+
       if(isCorrect){
+
         if(currentIndex === gameQuestions.length - 1){
           setStatus("winner");
+
         }else{
-          console.log("correct");
           setCurrentIndex((prev) => prev + 1);
-          return(
-            <>
-            <h3>Tocan odgovor</h3>
-            </>
-          );
+          setSelectedId(null);
+          setReveal(false);
         }
+
       }else{
-        console.log("wrong")
         setStatus("finished");
       }
-    }, 1000);
+    }, 1200);
   };
 
   const currentQuestion = gameQuestions[currentIndex];
+
+  const getAnswerClass = (a) => {
+    if(!reveal) return "answer"
+    if(a.correct)return "answer correct";
+    if(selectedId === a.id)return "answer wrong";
+    return "answer";
+  }
 
   return(
     <div>
         <h2>{currentIndex + 1}. {currentQuestion.text}</h2>
         {currentQuestion.answers.map((a) => (
-          <button key={a.id} onClick={() => handleAnswer(a)}>{a.text}</button>
+          <button
+            key={a.id}
+            className={getAnswerClass(a)}
+            onClick={() => handleAnswer(a)}
+            disabled={reveal}
+          >{a.text}</button>
         ))}
     </div>
   );
