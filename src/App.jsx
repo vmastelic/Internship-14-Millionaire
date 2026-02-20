a0dc87041df370d1e2dcd51ad1cd4da993d260f1
Vatroslav Mastelić <vmaste00@fesb.hr>
2026-02-20 10:08:06 +0100

    Add start page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import './App.css'
+import { QUESTIONS } from './data/questions'
+
+
+function randomQuestions(questions){
+  const shuffled = [...questions].sort(() => Math.random() - 0.5 );
+  return shuffled.slice(0, 10);
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [status, setStatus] = useState("start");
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  return (
-    <>
-    </>
-  )
+  const gameQuestions = useMemo(() => randomQuestions(QUESTIONS), [status]);
+
+  const startGame = () => {
+    setCurrentIndex(0);
+    setStatus("playing");
+  }
+
+  if(status === "start"){
+    return(
+      <>
+        <h1>Dobrodošli u (polu)milijunaš</h1>
+        <button onClick={startGame}>Start game</button>
+      </>
+    );
+  }
+
 }
 
 export default App
